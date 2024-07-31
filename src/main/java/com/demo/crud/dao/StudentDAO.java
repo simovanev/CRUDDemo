4502464179bf327e45f7b4cb3113058fff807f58
Simovanev <simovanev@abv.bg>
2024-07-31 05:48:17 +0300

    Persist Student entity by EntityManager

---
+++ src/main/java/com/demo/crud/dao/StudentDAO.java
@@ -0,0 +1,7 @@
+package com.demo.crud.dao;
+
+import com.demo.crud.entity.Student;
+
+public interface StudentDAO {
+    void save(Student student);
+}
---
+++ src/main/java/com/demo/crud/dao/StudentDAOImpl.java
@@ -0,0 +1,21 @@
+package com.demo.crud.dao;
+
+import com.demo.crud.entity.Student;
+import jakarta.persistence.EntityManager;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+@Repository
+public class StudentDAOImpl implements StudentDAO {
+    private final EntityManager entityManager;
+
+    public StudentDAOImpl(EntityManager entityManager) {
+        this.entityManager = entityManager;
+    }
+
+    @Override
+    @Transactional
+    public void save(Student student) {
+        entityManager.persist(student);
+    }
+}
