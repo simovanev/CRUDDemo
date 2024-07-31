0be122419616563ec2454dda4f618d44a763fd7c
Simovanev <simovanev@abv.bg>
2024-07-31 05:58:30 +0300

    Create student

---
+++ src/main/java/com/demo/crud/CrudApplication.java
@@ -0,0 +1,32 @@
+package com.demo.crud;
+
+import com.demo.crud.dao.StudentDAO;
+import com.demo.crud.entity.Student;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class CrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudApplication.class, args);
+	}
+
+	@Bean
+	public CommandLineRunner commandLineRunner(StudentDAO studentDAO){
+		return runner ->{
+			createStudent(studentDAO);
+		};
+	}
+
+	private void createStudent(StudentDAO studentDAO) {
+		System.out.println("Creating student");
+		Student student = new Student("Paul", "Walker", "fastEndFurious@mail.com");
+
+		System.out.println("Saving student");
+		studentDAO.save(student);
+
+	}
+}
